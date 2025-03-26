003b5ea9400f67b09658901b4208b880773dc237
Josue LinarezHein <yosuskirnoff3@gmail.com>
2025-03-26 15:24:04 -0300

    Mejorado el manejo de argumentos en el cálculo de factorial

--- src/factorial/factorial.py
+++ src/factorial/factorial.py
@@ -6,22 +6,24 @@
 #* Creative commons                                                        *
 #*-------------------------------------------------------------------------*
 import sys
+
 def factorial(num): 
     if num < 0: 
         print("Factorial de un número negativo no existe")
         return 0
     elif num == 0: 
         return 1
-        
     else: 
         fact = 1
-        while(num > 1): 
+        while num > 1: 
             fact *= num 
             num -= 1
         return fact 
 
-if len(sys.argv) == 0:
-    print("Debe informar un número!")
-    sys.exit()
-num=int(sys.argv[1])
-print("Factorial ",num,"! es ", factorial(num)) 
+# Verificar si se pasó un argumento
+if len(sys.argv) < 2:
+    num = int(input("Ingrese un número para calcular su factorial: "))
+else:
+    num = int(sys.argv[1])
+
+print(f"Factorial de {num}! es {factorial(num)}")
